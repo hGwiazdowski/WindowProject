b7825318b30ab9cccfa84e830e7c4c4496e9c047
hgwiazdowski <hgwiazdowski@sii.pl>
2021-09-25 17:54:42 +0200

    Dodano Animal.java

---
+++ src/com/company/Animal.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Animal {
+    int legs;
+    String voice;
+    boolean canFly;
+
+    public void getVoice(){
+        System.out.println(voice);
+    }
+}
+
+
